1eacc3c9d13b1bea67ddcc14224e13ceab8f1841
Dan Boscaro <dan.boscaro@engineering.digital.dwp.gov.uk>
2018-11-05 11:53:47 +0000

    add renewal date page

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 
 const questionPages = [
     'name',
     'date-of-birth',
     'postcode',
     'urn',
+    'renewal-date',
     'changes',
     'contact'
 ].map(p => `/renew/${p}`);
@@ -14,6 +16,7 @@ router.use((req, res, next) => {
     res.locals.firstQuestion = questionPages[0];
     res.locals.questionNumber = questionPages.indexOf(req.path) + 1;
     res.locals.totalQuestions = questionPages.length;
+    res.locals.renewalDateExample = moment().add(3, 'weeks').format('D M YYYY');
     if(req.path === '/declaration'){
         res.locals.back = questionPages[questionPages.length - 1];
     } else {
