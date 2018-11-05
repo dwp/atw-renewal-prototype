a4e62270bffbf1290123bede95d73f46281b327e
Dan Boscaro <dan.boscaro@engineering.digital.dwp.gov.uk>
2018-11-05 09:00:48 +0000

    init

---
+++ app/routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const questionPages = [
+    'name',
+    'date-of-birth',
+    'postcode',
+    'urn',
+    'changes',
+    'contact'
+].map(p => `/renew/${p}`);
+
+router.use((req, res, next) => {
+    res.locals.firstQuestion = questionPages[0];
+    res.locals.questionNumber = questionPages.indexOf(req.path) + 1;
+    res.locals.totalQuestions = questionPages.length;
+    if(req.path === '/declaration'){
+        res.locals.back = questionPages[questionPages.length - 1];
+    } else {
+        res.locals.back = (questionPages.indexOf(req.path) === 0) ? '/' : questionPages[questionPages.indexOf(req.path) - 1]; 
+    }
+    next();
+})
+router.post('*', (req, res) => {
+    const nextPage = (questionPages.indexOf(req.path) !== questionPages.length - 1) ? questionPages[questionPages.indexOf(req.path)+1] : '/declaration';
+    res.status(302).redirect(nextPage);
+})
+
+module.exports = router
